31b7a01d00296a82696e5608ec7ad1889fe5f224
Александр Невский <sashnevski@MacBook-Air-Aleksandr.local>
2022-02-14 19:32:04 +0300

    init react ts project + base template for my app

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Layout from './components/layout/Layout';
+import Home from './components/pages/home/Home';
+import './index.css';
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Layout>
+      <Home />
+    </Layout>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
